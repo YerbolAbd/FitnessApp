e9ae3557ba094176c485e397d533ebe8681a02e7
AbdiyevYerbol <abdiyevyerbol@gmail.com>
2024-12-24 14:02:40 +0500

    First UI

---
+++ FitnessApp/FitnessTabView.swift
@@ -0,0 +1,18 @@
+//
+//  FitnessTabView.swift
+//  FitnessApp
+//
+//  Created by Ербол on 24.12.2024.
+//
+
+import SwiftUI
+
+struct FitnessTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FitnessTabView()
+}
---
+++ FitnessApp/ProgressCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressCircleView.swift
+//  FitnessApp
+//
+//  Created by Ербол on 24.12.2024.
+//
+
+import SwiftUI
+
+struct ProgressCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressCircleView()
+}
